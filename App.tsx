443e51871ca23ea352a9414f27b2055c2afbc1af
Leila Kasmi <leilakasmi150@gmail.com>
2023-12-13 20:04:49 +0100

    test

--- App.tsx
+++ App.tsx
@@ -1,70 +1,36 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import home from './app/home/home';
-import clas from './app/class/clas';
-import setting from './app/settings/setting';
-import { Constants } from 'expo-constants';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
-import { StatusBar } from 'expo-status-bar';
-import {StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Pressable, Image } from 'react-native';
+import Welcome from './app/Welcome';
+import index from './app/screens/index';
+
 
 
 
 const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style='auto' />
-      <NavigationContainer>
-        <Tab.Navigator
-        screenOptions={{headerShown: false}}
-        >
-          <Tab.Screen
-          name='Home'
-          component={home}
-          options={{
-            tabBarIcon:({color, size})=>(
-              <Ionicons name='home' size={size} color={color}/>
-            )
-          }}>
-          
-          </Tab.Screen>
-    
-          <Tab.Screen
-          name='Class'
-          component={clas}
-          options={{
-            tabBarIcon:({color, size})=>(
-              <Ionicons name='people' size={size} color={color}/>
-            )
-          }}>
-          
-          </Tab.Screen>
-        
-          <Tab.Screen
-          name='Settings'
-          component={setting}
-          options={{
-            tabBarIcon:({color, size})=>(
-              <Ionicons name='settings' size={size} color={color}/>
-            )
-          }}>
-          
-          </Tab.Screen>
-        </Tab.Navigator>
-      </NavigationContainer>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+      initialRouteName='Welcome'
+      >
+        <Stack.Screen 
+        name='Welcome'
+        component={Welcome}
+        options={{
+          headerShown: false
+        }}
+        />
+        <Stack.Screen 
+        name='Home'
+        component={index}
+        options={{
+          headerShown: false
+        }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-    // marginTop: Constants.statusBarHeight,
-    // tabBarShowLabel: false,
-    // headerShown: false,
-  },
-});
--- app/screens/index.tsx
+++ app/screens/index.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function setting() {
+const index = () => {
   return (
     <View>
-      <Text>setting</Text>
+      <Text>index</Text>
     </View>
   )
 }
+
+export default index
